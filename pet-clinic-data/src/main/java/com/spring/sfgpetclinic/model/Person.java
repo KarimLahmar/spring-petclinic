797f1a89c0f20a6e5fd9b8e1023fe67ce65e7e68
klahmar <k.lahmar@netfective.com>
2019-12-26 23:23:58 +0100

    add maven multi model

---
+++ pet-clinic-data/src/main/java/com/spring/sfgpetclinic/model/Person.java
@@ -0,0 +1,24 @@
+package com.spring.sfgpetclinic.model;
+
+public class Person {
+	
+	private String firstName;
+	private String lastnName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastnName() {
+		return lastnName;
+	}
+
+	public void setLastnName(String lastnName) {
+		this.lastnName = lastnName;
+	}
+
+}
